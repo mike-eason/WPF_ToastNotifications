b4696e6418d5d3129e07c9ffc6e63baf1653ad8c
unknown <mikee@DRESDEN.anagram.uk.com>
2015-06-04 11:51:15 +0100

    Init

---
+++ Toast.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace ToastNotifications
+{
+    internal class Toast
+    {
+        public DispatcherTimer Timer { get; private set; }
+        public ToastNotification Notification { get; private set; }
+
+        public Toast(ToastNotification notification)
+        {
+            Notification = notification;
+            Notification.AllowsTransparency = true;
+            Notification.WindowStyle = System.Windows.WindowStyle.None;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //Stop and close the window.
+            Timer.Stop();
+
+            Notification.Close();
+        }
+
+        public void Show(TimeSpan displayTime)
+        {
+            //Set up the timer
+            Timer = new DispatcherTimer();
+            Timer.Interval = displayTime;
+            Timer.Tick += Timer_Tick;
+
+            //Display the window
+            Notification.Show();
+
+            //Focus back to the other window if there is one present.
+            if (Notification.DisplayOrigin != null)
+                Notification.DisplayOrigin.Focus();
+
+            //Start the timer
+            Timer.Start();
+        }
+    }
+}
---
+++ ToastManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows;
+
+namespace ToastNotifications
+{
+    public enum ToastTypes
+    {
+        Success,
+        Error,
+        Warning,
+        Info
+    }
+
+    public enum DisplayLocations
+    {
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight
+    }
+
+    public static class ToastManager
+    {
+        /// <summary>
+        /// Displays a toast notification for 5 seconds.
+        /// </summary>
+        /// <param name="title">The toast title.</param>
+        /// <param name="message">The toast message.</param>
+        /// <param name="toastType">The type of toast to display.</param>
+        /// <param name="location">The location to display the toast.</param>
+        /// <param name="displayOrigin">The parent window to display the toast in.</param>
+        public static void Show(string title, string message,
+            ToastTypes toastType = ToastTypes.Error,
+            DisplayLocations location = DisplayLocations.BottomRight,
+            Window displayOrigin = null)
+        {
+            Show(title, message, new TimeSpan(0,0,5), toastType, location, displayOrigin);
+        }
+
+        /// <summary>
+        /// Displays a toast notification for a given length of time.
+        /// </summary>
+        /// <param name="title">The toast title.</param>
+        /// <param name="message">The toast message.</param>
+        /// <param name="displayTime">The length of time to display the toast for.</param>
+        /// <param name="toastType">The type of toast to display.</param>
+        /// <param name="location">The location to display the toast.</param>
+        /// <param name="displayOrigin">The parent window to display the toast in.</param>
+        /// <remarks>If displayOrigin is set, then the location will be within the window, as opposed to the screen.</remarks>
+        public static void Show(string title, string message, 
+            TimeSpan displayTime,
+            ToastTypes toastType = ToastTypes.Error,
+            DisplayLocations location = DisplayLocations.BottomRight,
+            Window displayOrigin = null)
+        {
+            //Create a new toast control
+            ToastNotification notification = new ToastNotification(title, message, toastType, location, displayOrigin);
+
+            //Create a toast and accompanying dispatcher timer.
+            Toast toast = new Toast(notification);
+
+            //display it for X seconds
+            toast.Show(displayTime);
+        }
+    }
+}
